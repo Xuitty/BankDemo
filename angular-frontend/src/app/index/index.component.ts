1677f378075171fe027d4a8923912691c7399512
Xuitty <ufokun@gmail.com>
2023-01-16 18:23:05 +0800

    Entity models are done, building index page.

---
+++ angular-frontend/src/app/index/index.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.css'],
+})
+export class IndexComponent {
+  title: string = '金發財商業銀行網路銀行';
+}
